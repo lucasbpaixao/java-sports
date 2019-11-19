89107af02c54399b7e63b99660ee00de84dbe827
VINICIUS PIRES LEOPOLDINO <vinicius.pleopoldino@senacsp.edu.br>
2019-11-19 18:31:51 -0300

    Minhas alterações

--- Projeto/src/Dao/ProdutoDao.java
+++ Projeto/src/Dao/ProdutoDao.java
@@ -4,23 +4,105 @@ import Model.ProdutoVO;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import Controller.CadastroClienteController;
+import DAOFactory.DAOFactory;
+import Model.CadastroClienteModel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
 
 public class ProdutoDao {
 
+    public List<String> Listar() {
+
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        try{
+            conexao = DAOFactory.conexao();
+            
+            instrucaoSQL = conexao.prepareStatement("SELECT * FROM produto ");
+            
+            ResultSet resultado = instrucaoSQL.executeQuery();
+            
+            List<ProdutoVO> produtos = new ArrayList<>();    
+            
+             while (resultado.next()){
+             
+                ProdutoVO produtus = new ProdutoVO(resultado.getString(""), 0, 0, 0)
+                 
+             }
+             
+             
+             
+             
+            
+        } catch (SQLException a) {
+            a.printStackTrace();
+        }
+       
+    
+
+        
+    }
+
     static ArrayList<ProdutoVO> produto = new ArrayList();
 
-    public void cadastroDeProdutos(ProdutoVO contato) {
-        contato.setId(produto.size() + 1);
-        produto.add(contato);
+    public String cadastroDeProdutos(ProdutoVO contato) {
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        try {
+            conexao = DAOFactory.conexao();
+
+            instrucaoSQL = conexao.prepareStatement("INSERT INTO produto (idProduto,nomeProduto,valor,quantidade) VALUES (?,?,?,?)");
+
+            instrucaoSQL.setString(2, contato.getProduto());
+            instrucaoSQL.setLong(3, (long) contato.getPreco());
+            instrucaoSQL.setLong(4, contato.getQuantidade());
+            instrucaoSQL.setInt(1, contato.getId());
+
+            instrucaoSQL.execute();
+
+        } catch (SQLException a) {
+            a.printStackTrace();
+        }
+        return "Cadastrado com sucesso";
     }
 
-    public String numeroDeCadastros() {
+//public String numeroDeCadastros() {
+    //return produto.size() + "";
+    //}
+    public void excluir(ProdutoVO excluir) {
 
-        return produto.size() + "";
-    }
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
 
-    public void excluir(int deletar) {
-        produto.remove(deletar);
+        try {
+            conexao = DAOFactory.conexao();
+
+            String SQL = "DELETE FROM produto (idProduto,nomeProduto,valor,quantidade) WHERE (?,?,?,?) ";
+
+            PreparedStatement Excluir = conexao.prepareCall(SQL);
+
+            Excluir.setString(2, excluir.getProduto());
+            Excluir.setLong(3, (long) excluir.getPreco());
+            Excluir.setLong(4, excluir.getQuantidade());
+            Excluir.setInt(1, excluir.getId());
+
+            Excluir.execute();
+
+        } catch (SQLException a) {
+            a.printStackTrace();
+        }
+        ;
+
     }
 
     public void alterar(int tirar, ProdutoVO telinha) {
@@ -69,3 +151,7 @@ public class ProdutoDao {
     }
 
 }
+
+//contato.setId(produto.size() + 1);
+//produto.add(contato);
+
--- Projeto/src/Model/ProdutoVO.java
+++ Projeto/src/Model/ProdutoVO.java
@@ -60,4 +60,6 @@ public class ProdutoVO {
         return produto + " : " + preco + " : " + quantidade;
 
     }
+    
+    
 }
